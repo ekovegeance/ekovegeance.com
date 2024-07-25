a34555342ef1a7e9b6322c77e320f7b003559b03
Eko <me@ekovegeance.com>
2024-07-25 12:56:24 +0700

    add vercel Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { icons } from "lucide-react";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+        </body>
     </html>
   );
 }
