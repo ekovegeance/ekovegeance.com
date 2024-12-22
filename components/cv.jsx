ac5f8a8190e052c00f681a967b1d741fb17bcd64
Eko <ekovegeance7@gmail.com>
2024-12-22 15:13:36 +0700

    refactor: Update Cv component to dynamically render data from external source

--- components/cv.jsx
+++ components/cv.jsx
@@ -4,21 +4,21 @@
  * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
  */
 import Link from "next/link";
+import { getData } from "../lib/data";
 
 export default function Cv() {
+  const data = getData();
   return (
     <div className="bg-background text-foreground font-inter">
       <div className="container max-w-4xl px-4 py-12 mx-auto sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
           <div className="col-span-1 md:col-span-1">
             <div className="flex flex-col items-start gap-4">
-              <div className="text-4xl font-bold">Eko Saputra</div>
+              <div className="text-4xl font-bold">{data.name}</div>
               <div className="grid gap-2 text-sm text-muted-foreground">
                 <div>
                   <MailIcon className="inline w-4 h-4 mr-2" />
-                  <Link href="mailto:me@ekovegeance.com">
-                    me@ekovegeance.com
-                  </Link>
+                  <Link href={`mailto:${data.email}`}>{data.email}</Link>
                 </div>
                 {/* <div>
                   <PhoneIcon className="inline w-4 h-4 mr-2" />
@@ -26,27 +26,27 @@ export default function Cv() {
                 </div> */}
                 <div>
                   <LocateIcon className="inline w-4 h-4 mr-2" />
-                  Palembang, Indonesia
+                  {data.address}
                 </div>
                 <div>
                   <LinkIcon className="inline w-4 h-4 mr-2" />
-                  <Link href="https://www.ekovegeance.com" prefetch={false}>
-                    www.ekovegeance.com
+                  <Link href={`https://${data.links.website}`} prefetch={false}>
+                    {data.links.website}
                   </Link>
                 </div>
                 <div>
                   <LinkIcon className="inline w-4 h-4 mr-2" />
                   <Link
-                    href="https://www.linkedin.com/in/ekovegeance/"
+                    href={`https://www.${data.links.linkedin}`}
                     prefetch={false}
                   >
-                    linkedin.com/in/ekovegeance/
+                    {data.links.linkedin}
                   </Link>
                 </div>
                 <div>
                   <LinkIcon className="inline w-4 h-4 mr-2" />
-                  <Link href="https://github.com/ekovegeance" prefetch={false}>
-                    github.com/ekovegeance
+                  <Link href={`https://${data.links.github}`} prefetch={false}>
+                    {data.links.github}
                   </Link>
                 </div>
               </div>
@@ -58,264 +58,98 @@ export default function Cv() {
                 <div className="mb-2 text-xl font-bold">
                   Ringkasan Profesional
                 </div>
-                <p className="text-muted-foreground">
-                  UI/UX Designer dan Front-End Developer dengan lebih dari 1
-                  tahun pengalaman dalam membangun aplikasi web modern dan
-                  desain antarmuka yang intuitif. Terampil menggunakan teknologi
-                  terbaru seperti React.js, Next.js, dan Figma. Memiliki
-                  kemampuan kolaborasi tim yang kuat, berfokus pada kualitas
-                  kode dan pengalaman pengguna, serta berkomitmen untuk
-                  menghasilkan solusi yang efektif dan user-friendly.
-                </p>
+                <p className="text-muted-foreground">{data.summary}</p>
               </div>
               <div>
                 <div className="mb-2 text-xl font-bold">Pendidikan</div>
                 <div className="grid gap-4">
                   <div>
                     <div className="mb-1 text-lg font-semibold">
-                      Universitas Bina Darma Palembang
+                      {data.education[0].institution}
                     </div>
                     <div className="text-sm text-muted-foreground">
-                      Sarjana Komputer (S.Kom)
+                      {data.education[0].degree}
                     </div>
                     <div className="text-sm text-muted-foreground">
-                      Teknik Informatika | 2020 - 2024
+                      {data.education[0].field} | {data.education[0].date}
                     </div>
                     <div className="text-sm text-muted-foreground">
-                      IPK: 3.77
+                      IPK: {data.education[0].gpa}
                     </div>
                   </div>
                 </div>
               </div>
               <div>
                 <div className="mb-2 text-xl font-bold">Pengalaman Kerja</div>
-                <div className="grid gap-6">
-                  <div>
-                    <div className="mb-1 text-lg font-semibold">
-                      Front End Developer
+                {data.experiences.map((exp, index) => (
+                  <div key={index}>
+                    <div>
+                      <div className="mt-4 mb-1 text-lg font-semibold">
+                        {exp.title}
+                      </div>
+                      <div className="text-sm text-muted-foreground">
+                        {exp.company}
+                      </div>
+                      <div className="text-sm text-muted-foreground">
+                        <Link href={exp.link}>
+                          {exp.location} | {exp.date}
+                        </Link>
+                      </div>
+                      <ul className="mt-2 list-disc ms-4 text-muted-foreground">
+                        {exp.description.map((desc, index) => (
+                          <li key={index}>{desc}</li>
+                        ))}
+                      </ul>
                     </div>
-                    <div className="text-sm text-muted-foreground">
-                      Freelance
-                    </div>
-                    <div className="text-sm text-muted-foreground">
-                      <Link href="https://www.payoprint.id/">
-                        PT. Inaprint Lentera Teknologi Sriwijaya (payoprint) |
-                        Mei 2024 - Des 2024
-                      </Link>
-                    </div>
-                    <ul className="mt-2 list-disc ms-4 text-muted-foreground">
-                      <li>
-                        Mengembangkan dan memelihara aplikasi web yang kompleks
-                        menggunakan React, Node.js, dan teknologi pendukung
-                        lainnya.
-                      </li>
-                      <li>
-                        Berkolaborasi dengan tim untuk merancang dan
-                        mengimplementasikan fitur-fitur baru yang memperbaiki
-                        pengalaman pengguna.
-                      </li>
-                    </ul>
                   </div>
-                  <div>
-                    <div className="mb-1 text-lg font-semibold">
-                      UI Desinger
-                    </div>
-                    <div className="text-sm text-muted-foreground">
-                      Magang / Intern
-                    </div>
-                    <div className="text-sm text-muted-foreground">
-                      <Link href="https://www.payoprint.id/">
-                        PT. Inaprint Lentera Teknologi Sriwijaya (payoprint) |
-                        Oktober 2023 - Februari 2024
-                      </Link>
-                    </div>
-                    <ul className="mt-2 list-disc ms-4 text-muted-foreground">
-                      <li>
-                        Menciptakan antarmuka yang estetis, intuitif, dan
-                        responsif.
-                      </li>
-                      <li>
-                        Terampil menggunakan Figma untuk mendesain user flow
-                        yang efektif dan mendukung pengalaman pengguna yang
-                        optimal.
-                      </li>
-                      <li>
-                        Berfokus pada detail dan efisiensi dalam menghasilkan
-                        desain yang fungsional dan menarik.
-                      </li>
-                    </ul>
-                  </div>
-                  <div>
-                    <div className="mb-1 text-lg font-semibold">
-                      Project Intern
-                    </div>
-                    <div className="text-sm text-muted-foreground">
-                      Magang / Intern
-                    </div>
-                    <div className="text-sm text-muted-foreground">
-                      <Link href="https://loko.co.id/">
-                        LOKO Sistem Terintegrasi | Maret 2023 - Agustus 2023
-                      </Link>
-                    </div>
-                    <ul className="mt-2 list-disc ms-4 text-muted-foreground">
-                      <li>
-                        Terlibat dalam pengembangan Sistem informasi kampus yang
-                        kami kembangkan memiliki fitur-fitur penting, seperti
-                        manajemen data mahasiswa, jadwal kuliah, penilaian, dan
-                        laporan akademik.
-                      </li>
-                    </ul>
-                  </div>
-                  <div>
-                    <div className="mb-1 text-lg font-semibold">
-                      Support Team Lead
-                    </div>
-                    <div className="text-sm text-muted-foreground">
-                      Magang / Intern
-                    </div>
-                    <div className="text-sm text-muted-foreground">
-                      <Link href="https://loko.co.id/">
-                        LOKO Sistem Terintegrasi | Maret 2023 - Agustus 2023
-                      </Link>
-                    </div>
-                    <ul className="mt-2 list-disc ms-4 text-muted-foreground">
-                      <li>
-                        Memimpin pengembangan website Skodi (Sekolah Koding
-                        Indonesia), sebuah platform pembelajaran coding online.
-                      </li>
-                      <li>
-                        Fokus pada kolaborasi tim, pengembangan fitur, dan
-                        integrasi sistem yang efektif.
-                      </li>
-                    </ul>
-                  </div>
-                  <div>
-                    <div className="mb-1 text-lg font-semibold">
-                      Teknisi Komputer
-                    </div>
-                    <div className="text-sm text-muted-foreground">
-                      Magang / Intern
-                    </div>
-                    <div className="text-sm text-muted-foreground">
-                      <Link href="https://www.google.com/search?sca_esv=fa65bd0a363ad061&sxsrf=ADLYWIJkajJocILIlWyEvQWF1cP29Qy4Yw:1730991129725&q=cahaya+komputer+pariaman&source=lnms&fbs=AEQNm0AeMNWKf4PpcKMI-eSa16lJoRPMIuyspCxWO6iZW9F1Ns6EVsgc0W_0xN47PHaanAEtg26fpfc9gg2y1-ZsywNNvFNsUhDWR4E0wi7xGg-xrf_Sy9bzTMgYml2XYO8-wGBzdYyjgm1GxUgrzQRgc4dMk-rxWBBU74weqHI1oXOBcIzqHLRs2wmN3kdZa-sR8S7dDD-YaL4NK2a5JkTHeKoYpFindg&sa=X&ved=2ahUKEwjp6ZW7vMqJAxUayzgGHbkWF8IQ0pQJegQICBAB&biw=1333&bih=652&dpr=2">
-                        Cahaya Komputer Pariaman | Juli 2017 - Desember 2017
-                      </Link>
-                    </div>
-                    <ul className="mt-2 list-disc ms-4 text-muted-foreground">
-                      <li>
-                        Melakukan perbaikan komputer, laptop, dan printer,
-                        instalasi sistem operasi, serta pemasangan software
-                        pendukung.
-                      </li>
-                      <li>
-                        Berfokus pada troubleshooting dan memastikan perangkat
-                        berfungsi optimal.
-                      </li>
-                    </ul>
-                  </div>
-
-                  {/* <div>
-                    <div className="mb-1 text-lg font-semibold">
-                      Web Developer Intern
-                    </div>
-                    <div className="text-sm text-muted-foreground">
-                      Widgets Inc. | Jun 2018 - Aug 2019
-                    </div>
-                    <p className="mt-2 text-muted-foreground">
-                      Assisted in the development and maintenance of the
-                      company's website and web-based tools. Gained experience
-                      in front-end and back-end technologies, including HTML,
-                      CSS, JavaScript, and PHP.
-                    </p>
-                  </div> */}
-                </div>
+                ))}
               </div>
               <div>
                 <div className="mb-2 text-xl font-bold">
                   Keterampilan Teknis
                 </div>
                 <div className="grid grid-cols-2 gap-2 sm:grid-cols-3">
-                  <div className="px-3 py-1 text-sm font-medium rounded-md bg-muted text-muted-foreground">
-                    JavaScript
-                  </div>
-                  <div className="px-3 py-1 text-sm font-medium rounded-md bg-muted text-muted-foreground">
-                    React
-                  </div>
-                  <div className="px-3 py-1 text-sm font-medium rounded-md bg-muted text-muted-foreground">
-                    Node.js
-                  </div>
-                  <div className="px-3 py-1 text-sm font-medium rounded-md bg-muted text-muted-foreground">
-                    HTML/CSS
-                  </div>
-                  <div className="px-3 py-1 text-sm font-medium rounded-md bg-muted text-muted-foreground">
-                    Git
-                  </div>
-                  <div className="px-3 py-1 text-sm font-medium rounded-md bg-muted text-muted-foreground">
-                    SQL
-                  </div>
-                  <div className="px-3 py-1 text-sm font-medium rounded-md bg-muted text-muted-foreground">
-                    Problem-solving
-                  </div>
-                  <div className="px-3 py-1 text-sm font-medium rounded-md bg-muted text-muted-foreground">
-                    Teamwork
-                  </div>
-                  <div className="px-3 py-1 text-sm font-medium rounded-md bg-muted text-muted-foreground">
-                    Communication
-                  </div>
+                  {data.skills.map((skill, index) => (
+                    <div
+                      key={index}
+                      className="px-3 py-1 text-sm font-medium rounded-md bg-muted text-muted-foreground"
+                    >
+                      {skill}
+                    </div>
+                  ))}
                 </div>
               </div>
               <div>
                 <div className="mb-1 text-xl font-semibold">
                   Proyek Terpilih
                 </div>
-                <ul className="mt-2 list-disc ms-4 text-muted-foreground">
-                  <li>
-                    DracOS GNU/Linux RemasteredDracOS GNU/Linux Remastered -
-                    Berkontribusi dalam pengembangan DracOS GNU/Linux
-                    Remastered, sebuah distro Linux open-source yang dibangun
-                    berdasarkan proyek Debian Live. Didesain untuk pengujian
-                    keamanan (penetration testing), DracOS menyediakan berbagai
-                    alat untuk pentest, forensik, dan rekayasa balik, dengan
-                    dukungan GUI dan CLI.
-                  </li>
-                </ul>
+                {data.projects.map((project, index) => (
+                  <div key={index}>
+                    <div className="mt-4 mb-1 text-lg font-semibold">
+                      {project.title}
+                    </div>
+                    <div className="text-muted-foreground">
+                      {project.description}
+                    </div>
+                  </div>
+                ))}
               </div>
 
               <div>
                 <div className="mb-1 text-xl font-semibold">
                   Sertifikat dan Penghargaan
                 </div>
-                <ul className="mt-2 list-disc ms-4 text-muted-foreground">
-                  <li>
-                    Alibaba Cloud Certified Developer - Alibaba Cloud Academy
-                    (2023)
-                  </li>
-                  <li>
-                    Juara 1 ITI - 1st Palembang Business Plan Award (2022)
-                  </li>
-                  <li>
-                    TryHackMe Pre Security Learning Path - TryHackMe (2022)
-                  </li>
-                  <li>
-                    Memulai Pemrograman Dengan Python - Dicoding Indonesia
-                    (2021)
-                  </li>
-                  <li>
-                    Belajar Dasar Visualisasi Data - Dicoding Indonesia (2021)
-                  </li>
-                  <li>CCNA: Introduction to Networks - Cisco NetAcad (2021)</li>
-                  <li>
-                    Cloud Practitioner Essentials (Belajar Dasar AWS Cloud) -
-                    Dicoding Indonesia (2021)
-                  </li>
-                  <li>
-                    CCNA: Switching, Routing, and Wireless Essentials - Cisco
-                    NetAcad (2020)
-                  </li>
-                  <li>
-                    Certified Fundamentals of Digital Marketing - Google (2019)
-                  </li>
-                </ul>
+                {data.certificates.map((certificate, index) => (
+                  <ul
+                    key={index}
+                    className="mt-2 list-disc ms-4 text-muted-foreground"
+                  >
+                    <li>
+                      {certificate.title} - {certificate.issuer} (
+                      {certificate.date})
+                    </li>
+                  </ul>
+                ))}
               </div>
               <div>
                 <div className="mb-1 text-xl font-semibold">Bahasa</div>
@@ -398,45 +232,6 @@ function MailIcon(props) {
     >
       <rect width="20" height="16" x="2" y="4" rx="2" />
       <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
-    </svg>
-  );
-}
-
-function PhoneIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
-    </svg>
-  );
-}
-
-function XIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M18 6 6 18" />
-      <path d="m6 6 12 12" />
     </svg>
   );
 }
---
+++ lib/data.ts
@@ -0,0 +1,114 @@
+export function getData() {
+    const data = {
+        name: "Eko Saputra",
+        summary:
+            "UI/UX Designer dan Front-End Developer dengan lebih dari 1 tahun pengalaman dalam membangun aplikasi web modern dan desain antarmuka yang intuitif. Terampil menggunakan teknologi terbaru seperti React.js, Next.js, dan Figma. Memiliki kemampuan kolaborasi tim yang kuat, berfokus pada kualitas kode dan pengalaman pengguna, serta berkomitmen untuk menghasilkan solusi yang efektif dan user-friendly.",
+        email: "me@ekovegeance",
+        address: "Palembang, Indonesia",
+        links: {
+            website: "www.ekovegeance.com",
+            linkedin: "linkedin.com/in/ekovegeance/",
+            github: "gitub.com/ekovegeance",
+        },
+        education: [
+            {
+                institution: "Universitas Bina Darma Palembang",
+                degree: "Sarjana Komputer (S.Kom)",
+                field: "Teknik Informatika",
+                date: "2020 - 2024",
+                gpa: "3.77",
+            },
+        ],
+        experiences: [
+            {
+                title: "Front End Developer",
+                company: "Freelance",
+                link: "https://www.payoprint.id/",
+                location: "PT. Inaprint Lentera Teknologi Sriwijaya (payoprint)",
+                date: "Mei 2024 - Des 2024",
+                description: [
+                    "Mengembangkan dan memelihara aplikasi web yang kompleks menggunakan React, Node.js, dan teknologi pendukung lainnya.",
+                    "Berkolaborasi dengan tim untuk merancang dan mengimplementasikan fitur-fitur baru yang memperbaiki pengalaman pengguna.",
+                ],
+            },
+            {
+                title: "UI Desinger",
+                company: "Magang / Intern",
+                link: "https://www.payoprint.id/",
+                location: "PT. Inaprint Lentera Teknologi Sriwijaya (payoprint)",
+                date: "Oktober 2023 - Februari 2024",
+                description: [
+                    "Menciptakan antarmuka yang estetis, intuitif, dan responsif.",
+                    "Terampil menggunakan Figma untuk mendesain user flow yang efektif dan mendukung pengalaman pengguna yang optimal.",
+                    "Berfokus pada detail dan efisiensi dalam menghasilkan desain yang fungsional dan menarik.",
+                ],
+            },
+        ],
+        skills: [
+            "JavaScript",
+            "React",
+            "Node.js",
+            "HTML/CSS",
+            "Git",
+            "SQL",
+            "Problem-solving",
+            "Teamwork",
+            "Communication",
+        ],
+        projects: [
+            {
+                title: "DracOS GNU/Linux Remastered",
+                description:
+                    "Berkontribusi dalam pengembangan DracOS GNU/Linux Remastered, sebuah distro Linux open-source yang dibangun berdasarkan proyek Debian Live. Didesain untuk pengujian keamanan (penetration testing), DracOS menyediakan berbagai alat untuk pentest, forensik, dan rekayasa balik, dengan dukungan GUI dan CLI.",
+            },
+        ],
+        certificates: [
+            {
+                title: "Alibaba Cloud Certified Developer",
+                issuer: "Alibaba Cloud Academy",
+                date: "2023",
+            },
+            {
+                title: "1st Palembang Business Plan Award",
+                issuer: "ITI",
+                date: "2022",
+            },
+            {
+                title: "Pre Security Learning Path",
+                issuer: "TryHackMe",
+                date: "2022",
+            },
+            {
+                title: "Memulai Pemrograman Dengan Python",
+                issuer: "Dicoding Indonesia",
+                date: "2021",
+            },
+            {
+                title: "Belajar Dasar Visualisasi Data",
+                issuer: "Dicoding Indonesia",
+                date: "2021",
+            },
+            {
+                title: "Introduction to Networks",
+                issuer: "Cisco NetAcad",
+                date: "2021",
+            },
+            {
+                title: "Cloud Practitioner Essentials",
+                issuer: "Dicoding Indonesia",
+                date: "2021",
+            },
+            {
+                title: "Switching, Routing, and Wireless Essentials",
+                issuer: "Cisco NetAcad",
+                date: "2020",
+            },
+            {
+                title: "Fundamentals of Digital Marketing",
+                issuer: "Google",
+                date: "2019",
+            },
+        ]
+    };
+    return data;
+}
