0e820fdf26575d49d7bbf9bd705c831677505ca5
Eko <ekovegeance7@gmail.com>
2024-12-23 21:39:14 +0700

    feat: Enhance BioLink and Book components to dynamically fetch and display user and book data
    - Updated BioLink component to utilize dynamic data for user information (name, username, image) from the new getData function.
    - Refactored Book component to dynamically render a list of books using the new getBooks function, improving maintainability and scalability.
    - Removed hardcoded book entries for a more flexible approach to displaying book information.

--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,8 @@
 export function getData() {
     const data = {
         name: "Eko Saputra",
+        username: "ekovegeance",
+        image: "https://avatars.githubusercontent.com/u/26839751?v=4",
         summary:
             "UI/UX Designer dan Front-End Developer dengan lebih dari 1 tahun pengalaman dalam membangun aplikasi web modern dan desain antarmuka yang intuitif. Terampil menggunakan teknologi terbaru seperti React.js, Next.js, dan Figma. Memiliki kemampuan kolaborasi tim yang kuat, berfokus pada kualitas kode dan pengalaman pengguna, serta berkomitmen untuk menghasilkan solusi yang efektif dan user-friendly.",
         email: "me@ekovegeance",
@@ -112,3 +114,51 @@ export function getData() {
     };
     return data;
 }
+
+export function getBooks() {
+    const books = [
+        {
+          title: "Refactoring UI",
+          author: "Adam Wathan & Steve Schoger",
+          genre: "Learning",
+          description:
+            "Panduan praktis untuk membangun tampilan antarmuka yang lebih baik dan lebih menarik.",
+        },
+        {
+          title: "Atomic Habits",
+          author: "James Clear",
+          genre: "Self-help",
+          description:
+            "Panduan praktis untuk membangun kebiasaan baik dan menghentikan kebiasaan buruk yang, berdasarkan penelitian terbaru di bidang psikologi dan ilmu saraf.",
+        },
+        {
+          title: "Debian GNU/Linux Bible",
+          author: "Debian",
+          genre: "Learning",
+          description:
+            "Debian GNU/Linux adalah salah satu distribusi Linux utama yang tersedia saat ini. Distribusi ini dikenal sebagai distribusi Linux yang paling terbuka -- karena komitmennya terhadap prinsip-prinsip perangkat lunak open source.",
+        },
+        {
+          title: "Linux From Scratch",
+          author: "Gerard Beekmans",
+          genre: "Learning",
+          description:
+            "Buku ini memberikan petunjuk kepada pembaca tentang cara membangun sistem Linux dari awal.",
+        },
+        {
+          title: "Pro Git",
+          author: "Scott Chacon, Ben Straub",
+          genre: "Learning",
+          description:
+            "Git adalah sistem kontrol versi yang dikembangkan oleh Linus Torvalds untuk pengembangan kernel Linux. Hal ini menggemparkan dunia open source sejak didirikan pada tahun 2005.",
+        },
+        {
+          title: "Tenggelamnya Kapal Van Der Wijck",
+          author: "Hamka",
+          genre: "Novel",
+          description:
+            "Berlatar belakang kehidupan di Minangkabau, tanah asal Buya Hamka, dengan masalah adat yang berlaku pada saat itu perihal warisan, perjodohan dan kawin paksa, serta pertalian darah dan status sosial yang sangat kuat berakar.",
+        }
+      ];
+    return books;
+    }
