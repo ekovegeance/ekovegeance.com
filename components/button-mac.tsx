9a17115a787c1938c5cd9f223f3ebc2d6724a5a7
Eko <ekovegeance7@gmail.com>
2025-02-04 21:34:21 +0700

    refactor: Update button styles in ButtonMac component for improved design consistency

--- components/button-mac.tsx
+++ components/button-mac.tsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function ButtonMac() {
   return (
     <div className="flex gap-2 mx-6 mt-4">
-      <button className="h-1 p-2 bg-red-500 rounded-full"></button>
-      <button className="h-1 p-2 bg-yellow-500 rounded-full"></button>
-      <button className="h-1 p-2 bg-green-500 rounded-full"></button>
+      <button className="w-3 h-3 rounded-full bg-zinc-600"></button>
+      <button className="w-3 h-3 rounded-full bg-zinc-500"></button>
+      <button className="w-3 h-3 rounded-full bg-zinc-400"></button>
     </div>
   );
 }
