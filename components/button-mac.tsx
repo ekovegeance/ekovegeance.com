bd745db6546843c753cb3b45ff18543e2e50b664
Eko <ekovegeance7@gmail.com>
2025-05-22 22:40:18 +0700

    fix(speedinsight): Accessibility

--- components/button-mac.tsx
+++ components/button-mac.tsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function ButtonMac() {
   return (
     <div className="flex gap-2 mx-6 mt-4">
-      <button className="w-3 h-3 bg-teal-500 rounded-full"></button>
-      <button className="w-3 h-3 bg-red-500 rounded-full"></button>
-      <button className="w-3 h-3 rounded-full bg-amber-500"></button>
+      <div className="w-3 h-3 bg-teal-500 rounded-full"></div>
+      <div className="w-3 h-3 bg-red-500 rounded-full"></div>
+      <div className="w-3 h-3 rounded-full bg-amber-500"></div>
     </div>
   );
 }
